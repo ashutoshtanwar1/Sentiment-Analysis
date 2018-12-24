cc834e3ec38f0b4466f97d08ad1c82bbaacb1236
Ashutosh <ashutoshtanwar1@gmail.com>
2018-12-25 02:01:43 +0530

    committed!

--- temp.py
+++ temp.py
@@ -4,11 +4,11 @@ from flask import Flask, render_template,request
 app = Flask(__name__)
 
 # Importing the libraries
-import pandas as pd
+import pandas
 import numpy as np
 
 # Importing the dataset
-dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+dataset = pandas.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
 # Cleaning the texts
 import re
